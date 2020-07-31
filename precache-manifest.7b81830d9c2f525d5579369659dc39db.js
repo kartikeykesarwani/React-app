0131577e9b1403994b7c2861a96f42649951506a
kartikeykesarwani <kartikeykesarwani.2@gmail.com>
2020-08-01 00:58:42 +0530

    Updates

---
+++ precache-manifest.7b81830d9c2f525d5579369659dc39db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c6ee3bda5a06dec54758d2c39de4b0",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "ed9b6b7b3f5e1db31da5",
+    "url": "/Robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c346c4cdd3c2bc1d069f",
+    "url": "/Robofriends/static/css/main.b8451ebf.chunk.css"
+  },
+  {
+    "revision": "ed9b6b7b3f5e1db31da5",
+    "url": "/Robofriends/static/js/2.12e19136.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.12e19136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c346c4cdd3c2bc1d069f",
+    "url": "/Robofriends/static/js/main.f97417ab.chunk.js"
+  },
+  {
+    "revision": "351c2394620c1f8f70b1",
+    "url": "/Robofriends/static/js/runtime-main.32cacffe.js"
+  }
+]);
